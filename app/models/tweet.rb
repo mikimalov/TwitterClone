da82225857cbb1fdd77b5cfd852a67e9092e07f9
Miki <miki.stomatologija@gmail.com>
2020-05-26 23:06:40 +0200

    Generated Tweet model

---
+++ app/models/tweet.rb
@@ -0,0 +1,3 @@
+class Tweet < ApplicationRecord
+  validates :title, presence: true, length: {minimum: 5}
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  root to: 'tweets#index'
+  get '/tweets', to: 'tweets#index'
+  post '/tweets', to: 'tweets#create'
+  get '/tweets/new', to: 'tweets#new', as: :new_tweet
+  get '/tweets/:id/edit', to: 'tweets#edit', as: :edit_tweet
+  get '/tweets/:id', to: 'tweets#show', as: :tweet
+  patch '/tweets/:id', to: 'tweets#update'
+  delete 'tweets/:id', to: 'tweets#destroy'
+  # resources :tweets
+end
